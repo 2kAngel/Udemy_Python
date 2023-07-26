59c4ed5b1922b529ace1aa7f0e11db7a51ae5126
ÁNGEL CÁMARA RAMOS <angelcamararamos@hotmail.com>
2023-07-26 17:49:23 +0200

    INICIO GRISES

--- escalada_grises.py
+++ escalada_grises.py
@@ -1,5 +1,9 @@
 import cv2
 imagen = cv2.imread('C:\\Users\\ANGEL\\Desktop\\Angel\\OutUni\\phyton\\Udemy\\Udemy_Python\\contorno.jpg')
-cv2.imshow('imagen', imagen)
+gris = cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
+
+
+cv2.imshow('Imagen a Gris:', gris)
+cv2.imshow('Imagen a Original:', imagen)
 cv2.waitKey(0) #1-> video | 0->img
 cv2.destroyAllWindows()
